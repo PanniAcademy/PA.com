4e082be7168545874196b0639060ba54439ba914
PanniAcademy <panniacademy@gmail.com>
2025-01-25 15:39:15 +0530

    Create script.js

---
+++ Js files/script.js
@@ -0,0 +1,14 @@
+// Open Sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+// Close Sidebar
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+
+// Toggle Dark Mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
